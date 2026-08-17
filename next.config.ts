abecbd5406d667463905e542d8e9a9d48eba2365
averroes96 <addavigner@gmail.com>
2026-08-17 17:56:30 +0100

    chore: remove standalone output configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   // Allow accessing the dev server from another device on the local Wi-Fi network (like a phone)
   // @ts-ignore - Next.js internal type missing in some versions
   allowedDevOrigins: ['192.168.1.16'],
-  output: 'standalone',
 };
 
 export default nextConfig;
